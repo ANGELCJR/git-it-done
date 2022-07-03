c1f4ec13e56b30a62d7a1faf102833523683c585
Your Name <angeltcastillo11@gmail.com>
2022-07-03 11:13:14 -0500

    post

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -1,11 +1,22 @@
 var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
     });
-  };
+  });
+};
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+var userFormEl = document.querySelector("user-form");
+var nameInputEl = document.querySelector("username");
+
+//formSubmitHandler
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+};
